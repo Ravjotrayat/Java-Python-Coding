f4eee04f73e82c0f728052966745ef37604a5c5f
Ravjot Singh Rayat <94758593+Ravjotrayat@users.noreply.github.com>
2023-04-13 12:17:04 +0530

    Update and rename A2.java to Factors.java

--- Assignment 3/Factors.java
+++ Assignment 3/Factors.java
@@ -1,7 +1,7 @@
 //  Input a number and print all the factors
 //  of that number (use loops).
 import java.util.*;
-public class A2
+public class Factors
 {
     public static void main(String args[])
    {
