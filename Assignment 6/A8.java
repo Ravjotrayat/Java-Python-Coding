860fee3b1867ad48ebed30ac24f3574c5793095d
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2023-07-05 08:57:51 +0530

    Java

---
+++ Assignment 6/A8.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class A8
+{
+  public static void main(String args[])
+  {
+  int grid[][]={{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+	int c=countNegatives(grid);
+  System.out.println(c);
+  }
+    public static int countNegatives(int[][] grid) 
+    {
+         int count=0;
+
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j]<0)
+                   { count++;  }
+                
+            }
+        }
+        return count;
+    }
+} 
