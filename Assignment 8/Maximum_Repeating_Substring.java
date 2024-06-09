95e8bc2cc2b71c9611028d2fad919c4106989142
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2024-06-09 11:23:23 +0530

    Java

--- Assignment 8/Maximum_Repeating_Substring.java
+++ Assignment 8/Maximum_Repeating_Substring.java
@@ -1,24 +1,17 @@
-// Maximum Repeating Substring
+//Maximum Repeating Substring
 
-import java.util.*;
 public class Maximum_Repeating_Substring
-{
-    public static int maxRepeating(String sequence, String word) {
-       int ans = 0;
-        int ls = sequence.length(), lw = word.length();
-        for (int i=0;i<=ls-lw;i++) {
-            int j=0;
-            for (;j<ls-i;j++) {
-                if (sequence.charAt(i+j)!=word.charAt(j%lw)) {break;}
-            }
-            ans=Math.max(ans,j/lw);
-        }
-        return ans;
-    }
 
-public static void main(String args[])
 {
-String sequence="ababc",word="ac";
-System.out.println(maxRepeating(sequence,wor));
+    public int maxRepeating(String sequence, String word) {
+        int k=0;
+        StringBuilder arr=new StringBuilder(word);
+        while(sequence.contains(arr))
+        {
+                k++;
+                arr.append(word); 
+        }
+        return k;
+    }
 }
-}
+
