8926ff902a8212d460dc70251d1402c825ebffc1
Ravjotrayat <rayat.ravjotsingh@gmail.com>
2025-05-26 22:24:20 +0530

    python

---
+++ Python/Assignment 5 (Arrays)/Running Sum of 1d Array.py
@@ -0,0 +1,21 @@
+# Running Sum of 1d Array
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        total=nums[0]
+        for i in range(1,len(nums)):
+            nums[i]+=total
+            total=nums[i]
+        return nums
+        
+def main():
+    nums=[1,2,3,4]
+    solution=Solution()
+    reuslt=solution.runningSum(nums)
+    print(reuslt)
+    
+if __name__ == "__main__":
+    main()
